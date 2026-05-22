cd9a07da00daa2911cb8f90052d01ba4074a3ecb
Mesa10GCS <vincenzoggggw@gmail.com>
2026-05-22 20:50:01 -0300

    Refactor main.c by cleaning up variables
    Removed unused variables and updated d7s_init parameters.

--- src/main.c
+++ src/main.c
@@ -2,17 +2,14 @@
 #include "tecladom.h"
 #include "d7s.h"
 #include "string.h"
-
-
-int led_verde = 12;
-int led_rojo = 11;
-
-char correcta[4]={'1','2','3','4'};
-char ingresado[4];
+int led_v = 14;
+int led_rojo = 15;
+char contra[4]={'1','2','3','4'};
+char ingresada[4];
 
 int main (void) {
     
-    RCC -> APB2ENR |= RCC_APB2ENR_IOPAEN;
+    RCC->APB2ENR|=RCC_APB2ENR_IOPAEN;
     
     GPIOA -> CRH &=~(0xF<<((led_rojo    %8)*4)); 
     GPIOA -> CRH |= (0x1<<((led_rojo    %8)*4));
@@ -20,13 +17,11 @@ int main (void) {
     GPIOA -> CRH &=~(0xF<<((led_verde   %8)*4)); 
     GPIOA -> CRH |= (0x1<<((led_verde   %8)*4));
 
-
     tecladom_init (0, 1, 2, 3, 4, 5, 6, 7);
-    d7s_init(11, 10, 5, 6, 7, 8, 9);
+    d7s_init(8, 9, 10, 11, 12, 13, 5);
 
-    
     while (1){
-        char tecla =  tecladom(0, 1, 2, 3, 4, 5, 6, 7);
+        char  =  tecladom(0, 1, 2, 3, 4, 5, 6, 7);
         
         for(int i = 0; i<=4; i++){
             
